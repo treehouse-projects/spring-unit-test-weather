60d8deb3a432afb7d950cb0ebc53d77cac8b72eb
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-07-07 10:26:17 -0500

    Implemented test methods in FavoriteControllerTest

--- src/test/java/com/teamtreehouse/web/controller/FavoriteControllerTest.java
+++ src/test/java/com/teamtreehouse/web/controller/FavoriteControllerTest.java
@@ -1,6 +1,9 @@
 package com.teamtreehouse.web.controller;
 
+import com.teamtreehouse.domain.Favorite;
+import com.teamtreehouse.service.FavoriteNotFoundException;
 import com.teamtreehouse.service.FavoriteService;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +12,15 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.teamtreehouse.domain.Favorite.FavoriteBuilder;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 import static org.junit.Assert.*;
 
@@ -30,18 +42,47 @@ public class FavoriteControllerTest {
     @Test
     public void index_ShouldIncludeFavoritesInModel() throws Exception {
         // Arrange the mock behavior
+        List<Favorite> favorites = Arrays.asList(
+            new FavoriteBuilder(1L).withAddress("Chicago").withPlaceId("chicago1").build(),
+            new FavoriteBuilder(2L).withAddress("Omaha").withPlaceId("omaha1").build()
+        );
+        when(service.findAll()).thenReturn(favorites);
+
         // Act (perform the MVC request) and Assert results
+        mockMvc.perform(get("/favorites"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("favorite/index"))
+            .andExpect(model().attribute("favorites",favorites));
+        verify(service).findAll();
     }
 
     @Test
     public void add_ShouldRedirectToNewFavorite() throws Exception {
         // Arrange the mock behavior
+        doAnswer(invocation -> {
+            Favorite f = (Favorite)invocation.getArguments()[0];
+            f.setId(1L);
+            return null;
+        }).when(service).save(any(Favorite.class));
+
         // Act (perform the MVC request) and Assert results
+        mockMvc.perform(
+          post("/favorites")
+            .param("formattedAddress","chicago, il")
+            .param("placeId","windycity")
+        ).andExpect(redirectedUrl("/favorites/1"));
+        verify(service).save(any(Favorite.class));
     }
 
     @Test
     public void detail_ShouldErrorOnNotFound() throws Exception {
         // Arrange the mock behavior
+        when(service.findById(1L)).thenThrow(FavoriteNotFoundException.class);
+
         // Act (perform the MVC request) and Assert results
+        mockMvc.perform(get("/favorites/1"))
+            .andExpect(view().name("error"))
+                .andExpect(model().attribute("ex", Matchers.instanceOf(FavoriteNotFoundException.class)));
+        verify(service).findById(1L);
     }
 }
