922526a874f31781de054702c8b84062860624f9
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-07-07 13:41:48 -0500

    Implemented FavoriteDao tests

--- src/test/java/com/teamtreehouse/dao/FavoriteDaoTest.java
+++ src/test/java/com/teamtreehouse/dao/FavoriteDaoTest.java
@@ -3,12 +3,22 @@ package com.teamtreehouse.dao;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.teamtreehouse.Application;
+import com.teamtreehouse.domain.Favorite;
+import com.teamtreehouse.domain.User;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static com.teamtreehouse.domain.Favorite.FavoriteBuilder;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
@@ -20,4 +30,25 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 public class FavoriteDaoTest {
     @Autowired
     private FavoriteDao dao;
+
+    @Before
+    public void setup() {
+        User user = new User();
+        user.setId(1L);
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user,null));
+    }
+
+    @Test
+    public void findAll_ShouldReturnTwo() throws Exception {
+        assertThat(dao.findAll(),hasSize(2));
+    }
+
+    @Test
+    public void save_ShouldPersistEntity() throws Exception {
+        String placeId = "treehouseASKDFJASD#";
+        Favorite fave = new FavoriteBuilder()
+            .withPlaceId(placeId).build();
+        dao.saveForCurrentUser(fave);
+        assertThat(dao.findByPlaceId(placeId),notNullValue(Favorite.class));
+    }
 }
